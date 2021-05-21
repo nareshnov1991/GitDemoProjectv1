c5eb25f06d99f37f7f4edee847b403cb82ba674e
Naresh <nareshboyaregi19@gmail.com>
2021-05-21 12:39:22 +0530

    code changes updated as per the TEAD lEAD..

--- com.hellospring/src/com/springhello/HelloBean.java
+++ com.hellospring/src/com/springhello/HelloBean.java
@@ -4,6 +4,7 @@ public class HelloBean {
 	
 	public String m1() {
 		System.out.println("Stromg Method ");
+		System.out.println("Added the new feature");
 		return "This is String method Return Type ";
 	}
 
